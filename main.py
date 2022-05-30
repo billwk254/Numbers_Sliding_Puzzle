6c5ef424b4b5b09f53981aa3a9a081789dd1664e
billwk254 <billkihungu51@gmail.com>
2022-05-30 16:20:10 +0300

    Added in a line to check if the file is being run directly and not imported

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import random, sys
 
 BLANK = "  "
 
+
 def main():
     """The main loop for the game"""
     try:
@@ -17,7 +18,7 @@ def main():
         *--*--*--*--* 
         |13|14|15|  |
         *--*--*--*--* """)
-        input("Press Enter to begin\n > ")
+        input("Press Enter(<--) to begin\n > ")
 
         gameboard = getnewpuzzle()
 
@@ -32,17 +33,19 @@ def main():
     except KeyboardInterrupt:
         print("\nThanks for playing! Try to finish the game the next time you play")
 
+
 def getnewboard():
     """Return a list of lists that represents a new tile puzzle."""
-    return [["1","5","9","13"],["2","6","10","14"],
-            ["3","7","11","15"],["4","8","12",BLANK]]
+    return [["1", "5", "9", "13"], ["2", "6", "10", "14"],
+            ["3", "7", "11", "15"], ["4", "8", "12", BLANK]]
+
 
 def displayboard(board):
     """Display the board onto the screen."""
-    labels = [board[0][0],board[1][0],board[2][0],board[3][0],
-              board[0][1],board[1][1],board[2][1],board[3][1],
-              board[0][2],board[1][2],board[2][2],board[3][2],
-              board[0][3],board[1][3],board[2][3],board[3][3]]
+    labels = [board[0][0], board[1][0], board[2][0], board[3][0],
+              board[0][1], board[1][1], board[2][1], board[3][1],
+              board[0][2], board[1][2], board[2][2], board[3][2],
+              board[0][3], board[1][3], board[2][3], board[3][3]]
 
     boardtodraw = """
     +---------+---------+---------+---------+
@@ -66,12 +69,14 @@ def displayboard(board):
     """.format(*labels)
     print(boardtodraw)
 
+
 def findblankspace(board):
     """Return an (x,y) tuple of the blank spaces location"""
     for x in range(4):
         for y in range(4):
             if board[x][y] == "  ":
-                return (x,y)
+                return (x, y)
+
 
 def askforplayermove(board):
     """Let the player select a tile to slide."""
@@ -84,29 +89,31 @@ def askforplayermove(board):
 
     while True:
         print("                          ({})".format(w))
-        print("Enter WASD (or Quit): ({}) ({}) ({})".format(a,s,d))
+        print("Enter WASD (or Quit): ({}) ({}) ({})".format(a, s, d))
 
         response = input("> ").upper()
         if response == "QUIT":
             sys.exit()
 
-        if response in (w + a + s + d).replace(" ",""):
+        if response in (w + a + s + d).replace(" ", ""):
             return response
         else:
             print("INVALID MOVE! (TRY AGAIN)")
 
-def makemove(board,move):
+
+def makemove(board, move):
     """Carry out the given move on the board"""
     bx, by = findblankspace(board)
 
     if move == "W":
-        board[bx][by],board[bx][by+1] = board[bx][by+1],board[bx][by]
+        board[bx][by], board[bx][by + 1] = board[bx][by + 1], board[bx][by]
     elif move == "A":
-        board[bx][by], board[bx+1][by] = board[bx+1][by], board[bx][by]
+        board[bx][by], board[bx + 1][by] = board[bx + 1][by], board[bx][by]
     elif move == "S":
-        board[bx][by], board[bx][by-1] = board[bx][by-1], board[bx][by]
-    elif move  == "D":
-        board[bx][by], board[bx-1][by] = board[bx-1][by], board[bx][by]
+        board[bx][by], board[bx][by - 1] = board[bx][by - 1], board[bx][by]
+    elif move == "D":
+        board[bx][by], board[bx - 1][by] = board[bx - 1][by], board[bx][by]
+
 
 def makerandommove(board):
     """Perform a slide in a random direction"""
@@ -123,6 +130,7 @@ def makerandommove(board):
 
     makemove(board, random.choice(validmoves))
 
+
 def getnewpuzzle(moves=200):
     """Get a new puzzle by making random slides from a solved state"""
     board = getnewboard()
@@ -131,5 +139,6 @@ def getnewpuzzle(moves=200):
         makerandommove(board)
     return board
 
-main()
 
+if __name__ == "__main__":
+    main()
